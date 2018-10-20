f8daf2aa369b5f5f1ee4e70fa96d874f44940f99
Harouna BA <you@example.com>
2018-10-20 13:50:41 +0000

    SYS

--- Myclass.java
+++ Myclass.java
@@ -2,6 +2,6 @@ public class   Main{
 
     public static void main(String[] args){
 int a= 5;
-       System.out.println("Hello class M1SIRSOIR");
+       System.out.println("Hello M1SIRSOIR");
     }
 }
