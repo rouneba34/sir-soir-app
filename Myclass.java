475b1acbf7dfba2d68ee00a35bd29d3d727a04a5
Harouna BA <you@example.com>
2018-10-20 13:19:09 +0000

    Addition

--- Myclass.java
+++ Myclass.java
@@ -3,5 +3,9 @@ public class   Main{
     public static void main(String[] args){
 
        System.out.println("Hello class M1SIRSOIR");
+	   int a = 5;
+	   int b = 7;
+	   int s = a+b;
+	   System.out.println("La somme est:"s);
     }
 }
